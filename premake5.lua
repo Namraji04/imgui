967662344537ee86a88c8fb40e11174697eeb0af
Naj <naji.amrani@outlook.fr>
2024-02-19 08:07:42 +0100

    creation own repo for imgui

--- premake5.lua
+++ premake5.lua
@@ -15,13 +15,14 @@ project "ImGui"
     "imstb_rectpack.h",
     "imstb_textedit.h",
     "imstb_truetype.h",
-    "imgui_demo.cpp"
+    "imgui_demo.cpp",
+    "imgui_tables.cpp" 
   }
 
-  filter "sustem:windows"
+  filter "system:windows"
   systemversion "latest"
   cppdialect "C++17"
   staticruntime "On"
 
-  filter { "system:windows", "configuration:Release" }
+  filter { "system:windows", "configurations:Release" }
   buildoptions "/MT"
